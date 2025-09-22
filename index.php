48c296a055feb1d55c1dff24bf211e4a3953891f
Weslleysantos12 <weslleymessiassantosdasilva@gmail.com>
2025-09-21 23:25:12 -0300

    Add files via upload
    2 layout de login criado

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="estilo/stily.css">
+    <title>Meu site</title>
+</head>
+<body>
+  
+  <div class="conteudo">
+       
+      <div class="imagem-lt"><img src="img/imagem1.png"></div>
+      <div class="login">
+      <form>
+       <h2>login</h2>
+       <div>
+        <label>e-mail<input type="email" name="user" placeholder="Email..." required></label>
+       </div><!--input text-->
+
+       <div>
+        <label>Password<input type="password" name="senha"  placeholder="Senha..." required></label>
+       </div><!--input password-->
+
+       <div>
+         <input type="submit" name="acao" Value="logar">
+       </div><!--input submit-->
+      </form><!--form-->
+      </div>
+  </div>
+</body>
+</html>
